08954664a29193b069a6dae58164d41b16ba15b2
Benjamin Andre <benjaminandre23@gmail.com>
2023-03-21 19:08:45 +0100

    made basic calc functions in app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+const userInput = document.querySelector('#user-input');
+const displayResult = document.querySelector('#result');
+
+const numberNuttons = document.querySelectorAll('[data-type="number"]');
+
+
+
+function add(a, b) {
+  return a + b;
+}
+
+function substract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function modulo(a, b) {
+  return a % b;
+}
+
